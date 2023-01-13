317acc09a50738af0155ec6db12e91a84c6283ed
Ashwin Kumar S <46064775+ashwinramuk@users.noreply.github.com>
2023-01-13 15:31:55 +0530

    bug fix

--- clock.js
+++ clock.js
@@ -6,6 +6,10 @@ let hour12format
 let minutes
 let seconds
 let meridiem
+let day
+let month
+let year
+let date
 let day_obj = {0:"Sunday",1:"Monday",2:"Tuesday",3:"Wednesday",4:"Thursday",5:"Friday",6:"Saturday"}
 let month_obj = {0:"January",1:"February",2:"March",3:"April",4:"May",5:"June",6:"July",7:"August",8:"September",9:"October",10:"November",11:"December"}
 let clock = setInterval(()=>{
