3679326c92a0e4b5f7d3f0edc556439076eb94e7
Ashwin Kumar S <ashwinramuk@gmail.com>
2023-01-13 15:22:15 +0530

    Bug fix

--- clock.js
+++ clock.js
@@ -7,7 +7,7 @@ let minutes
 let seconds
 let meridiem
 let day_obj = {0:"Sunday",1:"Monday",2:"Tuesday",3:"Wednesday",4:"Thursday",5:"Friday",6:"Saturday"}
-let month_obj = {0:"January"}
+let month_obj = {0:"January",1:"February",2:"March",3:"April",4:"May",5:"June",6:"July",7:"August",8:"September",9:"October",10:"November",11:"December"}
 let clock = setInterval(()=>{
     dateTime = new Date()
     hours = dateTime.getHours()
@@ -19,6 +19,6 @@ let clock = setInterval(()=>{
     month = dateTime.getMonth()
     year = dateTime.getFullYear()
     date = dateTime.getDate()
-    timeElement.innerText = `Time: ${hour12format?"0"+hour12format:hour12format}:${minutes}:${seconds} ${meridiem}`
-    dateElement.innerText = `Date: ${month_obj[month]} ${date} ${year} ${day_obj[day]}`
+    timeElement.innerText = `${hour12format?"0"+hour12format:hour12format}:${minutes<10?"0"+minutes:minutes}:${seconds<10?"0"+seconds:seconds} ${meridiem}`
+    dateElement.innerText = `${month_obj[month]} ${date} ${year} ${day_obj[day]}`
 },1000)
